86d48f3028ee4837027e0f0383e5d65a915f270e
AgataHru <potochkin2005@mail.ru>
2025-10-17 00:38:28 +0300

    Added Entities, Abstraction/BaseEntity, Enums for user role and types

---
+++ Backend/Backend/Abstraction/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Abstraction
+{
+    public abstract class BaseEntity
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DeletedAt { get; set; }
+    }
+}
---
+++ Backend/Backend/Entities/UserEntity.cs
@@ -0,0 +1,64 @@
+﻿using Backend.Abstraction;
+using Backend.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Entities
+{
+
+    public class UserEntity : BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //само генерируется
+        public Guid UserId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(30)]
+        public string Login { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress] //валидация
+        [StringLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(255)]
+        public string Password { get; set; } = string.Empty;
+
+        public UserRole Role { get; set; } = UserRole.User; //user, admin
+
+        public UserType Type { get; set; } = UserType.Platform; //platform, advertiser, both
+        
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Balance { get; set; }
+
+        // Связи _________________________
+        public List<ApplicationEntity>? Applications { get; set; }
+
+        public List<DealEntity>? Deals { get; set; }
+
+        public List<AdvertiserCategoryEntity>? AdvertiserCategories { get; set; }
+
+        public List<PlatformCategoryEntity>? PlatformCategories { get; set; }
+
+        public List<MessageEntity>? Messages { get; set; }
+
+        public List<FeedbackEntity>? Feedbacks { get; set; }
+        // _________________________
+
+        public string? AvatarPath { get; set; }
+
+        [MaxLength(600)]
+        public string? Bio { get; set; }
+
+        public List<string>? SocialLinks { get; set; } 
+        //позже можно добавить отдельный класс
+        //для классификации по названию платформы
+
+        public bool IsVerified { get; set; } = false;
+    }
+}
