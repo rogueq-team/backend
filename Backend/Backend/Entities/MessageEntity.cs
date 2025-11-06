4074ba12c8ba8178749001618be6f0cac95e92ee
AgataHru <potochkin2005@mail.ru>
2025-11-06 14:56:22 +0300

    Transactions, messages added

--- Backend/Backend/Entities/MessageEntity.cs
+++ Backend/Backend/Entities/MessageEntity.cs
@@ -1,6 +1,28 @@
-﻿namespace Backend.Entities
+﻿using Backend.Abstraction;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Entities
 {
-    public class MessageEntity
+    public class MessageEntity : BaseEntity
     {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid DealId { get; set; }
+
+        [Required]
+        public virtual DealEntity? Deal { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public virtual UserEntity? User { get; set; }
+
+        [Required]
+        [MaxLength(600)]
+        public string? Text { get; set; } = String.Empty;
     }
 }
--- Backend/Backend/Entities/TransactionEntity.cs
+++ Backend/Backend/Entities/TransactionEntity.cs
@@ -1,6 +1,23 @@
-﻿namespace Backend.Entities
+﻿using Backend.Abstraction;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Entities
 {
-    public class TransactionEntity
+    public class TransactionEntity : BaseEntity
     {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid DealId { get; set; }
+
+        [Required]
+        public virtual DealEntity? DealEntity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
     }
 }
